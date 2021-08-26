671e4e1ed51164eff08cb7ff357d1c95344ef344
viseth89 <viseth89@gmail.com>
2021-08-25 19:36:23 -0500

    1:30 mark in video, going over function and string objection

--- intro-to-typescript/build/index.js
+++ intro-to-typescript/build/index.js
@@ -1,5 +1,24 @@
 "use strict";
 console.log('Greetings');
-console.log('Greetings');
-console.log('Greetings');
-console.log('Greetings');
+var count = 5;
+count = 10;
+// count is a 'type' number
+var fname = "Justin";
+// const's type is a literal
+var testing;
+testing = 5;
+console.log(add(6, testing));
+var Student;
+var student = {
+    fname: 'Andy',
+    lname: 'Anderson',
+    grade: 12
+};
+function toString(student) {
+    return JSON.stringify(student);
+}
+function add(x, y) {
+    var result = x + y;
+    return result;
+}
+console.log(count);
--- intro-to-typescript/src/index.ts
+++ intro-to-typescript/src/index.ts
@@ -1,4 +1,41 @@
 console.log('Greetings')
-console.log('Greetings')
-console.log('Greetings')
-console.log('Greetings')
+
+let count = 5
+count = 10
+// count is a 'type' number
+
+const fname = "Justin"
+// const's type is a literal
+
+let testing: number;
+
+testing = 5
+
+console.log(add(6, testing))
+
+let Student: {
+    fname: string
+    lname: string
+    grade: number
+}
+
+let student = {
+    fname:'Andy',
+    lname:'Anderson',
+    grade:12
+}
+
+
+function toString(student:{
+    fname:string
+}){
+    return JSON.stringify(student)
+}
+
+function add(x: number, y: number): number {
+    let result = x+y
+    return result
+}
+
+
+console.log(count)
