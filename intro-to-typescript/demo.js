7d69e2829e8ec2aeae73059e95df50f8b1554b8c
viseth89 <viseth89@gmail.com>
2021-08-31 07:27:31 -0500

    1:23 Mark Getters and set. Another form of validation where we are checking if the password is a number if it is NOT we will NOT set the password

--- intro-to-typescript/demo.js
+++ intro-to-typescript/demo.js
@@ -16,17 +16,38 @@ class Animal {
 
 }
 
+class Tiger extends Animal {
+    _password = 42
+    constructor(name, legs, isDanger){
+        super(name,legs)
+        this.isDanger = isDanger
+    }
+
+    // getters
+    get password () {
+        return this._password
+    }
+
+    set password (n){
+        if(typeof n !== "number")
+            this._password = n
+    }
+}
+
+let tiger = new Tiger('bengal', 4, true)
+console.log(tiger)
+
 // Create an instance of a class
 
 let dog = new Animal("lab", 4)
 let cat = new Animal("ragdoll", 4)
 cat.legs
 cat.name
-cat.speak()
-console.log('viseth')
-console.log(dog, cat)
-console.log(dog.name, cat.legs)
-console.log(dog.name, cat.name)
+// cat.speak()
+// console.log('viseth')
+// console.log(dog, cat)
+// console.log(dog.name, cat.legs)
+// console.log(dog.name, cat.name)
 
 let numbers = [1,2,4]
 
