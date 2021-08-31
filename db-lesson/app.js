1aefee266d3273031e2882169bd5d2a535679fd7
viseth89 <viseth89@gmail.com>
2021-08-31 18:23:00 -0500

    Added one to one relationship

--- db-lesson/app.js
+++ db-lesson/app.js
@@ -1,19 +1,51 @@
 require('dotenv').config()
 
 
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_DBNAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     dialect: 'postgres'
 });
 
-
-((async () =>{
-    try {
-        await sequelize.authenticate();
-        console.log("Connection has been established successfully")
-    } catch (error){
-        console.error("Unable to connect to the database", error)
+const User = sequelize.define("User", {
+    username: {
+        type: DataTypes.STRING
     }
-})());
+})
+/* 
+One to One
+
+*/
+const Profile = sequelize.define("Profile", {
+    birthday: {
+        type: DataTypes.DATE
+    }
+})
+
+User.hasOne(Profile,{
+    onDelete: "CASCADE"
+});
+Profile.belongsTo(User)
+/* 
+One to Many
+
+*/
+
+/* 
+Many to Many
+
+*/
+
+;(async () => {
+    await sequelize.sync();
+})();
+
+// ((async () =>{
+//     try {
+//         await sequelize.sync();
+//         console.log("Connection has been established successfully")
+//     } catch (error){
+//         console.error("Unable to connect to the database", error)
+//     }
+// })());
